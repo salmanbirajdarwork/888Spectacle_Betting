df95362fe9dc7ce3fd9e07eab9d1cddb611fbbe3
salmanbirajdarwork <salmanbirajdarwork@gmail.com>
2020-11-19 14:11:08 +0000

    Betting API initial upload

---
+++ Sportsbooking/bookingsite/api/views.py
@@ -0,0 +1,74 @@
+from api.models import Match, Sport, Selection, Market
+from api.serializers import MatchListSerializer, MatchDetailSerializer
+
+from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework import status, viewsets
+from rest_framework.filters import OrderingFilter
+from rest_framework.response import Response
+
+
+class MatchViewSet(viewsets.ModelViewSet):
+
+    queryset = Match.objects.all()
+    serializer_class = MatchListSerializer # for list view
+    detail_serializer_class = MatchDetailSerializer # for detail view
+    filter_backends = (DjangoFilterBackend, OrderingFilter,)
+    ordering_fields = '__all__'
+
+
+    def get_serializer_class(self):
+
+        if self.action == 'retrieve':                     #retrieve: Return the given match
+            if hasattr(self, 'detail_serializer_class'):
+                return self.detail_serializer_class
+        return super().get_serializer_class()
+
+
+    def get_queryset(self):
+        """
+        Restricts the returned queries by filtering against
+        a `sport` and `name` query parameter in the URL.
+        """
+        queryset = Match.objects.all()
+        sport = self.request.query_params.get('sport', None)
+        name = self.request.query_params.get('name', None)
+        if sport is not None:
+            sport = sport.title()
+            queryset = queryset.filter(sport__name=sport)
+        if name is not None:
+            queryset = queryset.filter(name=name)
+        return queryset
+
+
+    def create(self, request):                     #   Create a new match instance or update odds
+
+        message = request.data.pop('message_type')
+
+        # check if incoming api request is for new event creation
+        if message == "NewEvent":
+            event = request.data.pop('event')
+            sport = event.pop('sport')
+            markets = event.pop('markets')[0] # for now we have only one market
+            selections = markets.pop('selections')
+            sport = Sport.objects.create(**sport)
+            markets = Market.objects.create(**markets, sport=sport)
+            for selection in selections:
+                markets.selections.create(**selection)
+            match = Match.objects.create(**event, sport=sport, market=markets)
+            return Response(status=status.HTTP_201_CREATED)
+
+        # check if incoming api request is for updation of odds
+        elif message == "UpdateOdds":
+            event = request.data.pop('event')
+            markets = event.pop('markets')[0]
+            selections = markets.pop('selections')
+            for selection in selections:
+                s = Selection.objects.get(id=selection['id'])
+                s.odds = selection['odds']
+                s.save()
+            match = Match.objects.get(id=event['id'])
+            return Response(status=status.HTTP_201_CREATED)
+
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
---
+++ Sportsbooking/bookingsite/mysite/urls.py
@@ -0,0 +1,21 @@
+from api import views
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
+
+
+router = DefaultRouter()
+router.register(r'match', views.MatchViewSet)
+
+schema_view = get_schema_view(title=' Sports Betting API',
+                description='API to bet matches and/or update odds.')
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('schema/', schema_view),
+    path('docs/', include_docs_urls(title='Bookings API'))
+]
